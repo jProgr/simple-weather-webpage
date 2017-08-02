aa6c0130056e7ab097dca7fb7a7e10871d3d9fcb
jProgr <8537691@gmail.com>
2017-08-02 16:59:35 -0500

    Add main page
    Renders main page with a form.
    
    - Server stuff will be handled by Express.
    - Added main page as EJS template.

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) =>
 {
-  res.send('Hello World!')
+  res.render('index');
 });
 
 app.listen(3000, function ()
 {
-  console.log('Example app listening on port 3000!')
+  console.log('Listening on port 3000');
 });
